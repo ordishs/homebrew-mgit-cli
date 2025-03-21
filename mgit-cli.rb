f6f945c3e834c3f7ba2a0890f5b7c9e08863e7d6
Simon Ordish <simon.ordish@masagi.co.uk>
2025-03-21 02:25:31 +0100

    v1.0.1

--- mgit-cli.rb
+++ mgit-cli.rb
@@ -1,9 +1,9 @@
 class MgitCli < Formula
   desc "Small utility to run git commands across all git sub folders"
   homepage "https://github.com/ordishs/mgit-cli"
-  url "https://github.com/ordishs/mgit-cli/releases/download/v1.0.0/mgit-cli-1.0.0.tar.gz"
-  sha256 "3b157326f597f3592899fc540ef96c5f3e812f6cc82b131f04838573e283b006"
-  version "1.0.0"
+  url "https://github.com/ordishs/mgit-cli/releases/download/v1.0.1/mgit-cli-1.0.1.tar.gz"
+  sha256 "b939143f01ff446a03841eca2c0553cb8a6b47475e9ad310792639fc30d98082"
+  version "1.0.1"
 
   def install
     bin.install "mgit-cli"
